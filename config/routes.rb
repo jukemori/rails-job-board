28c763f8b06a222ac2b49353b1a8cc89a5fda01d
Jun Ukemori <jukemori@gmail.com>
2023-07-15 19:49:21 +0900

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :boards do
+    resources :jobs, only: [:create, :update, :destroy]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
